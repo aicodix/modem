186954fd81dd8b8098a1a01c4229c518b086e99a
Ahmet Inan <xdsopl@gmail.com>
2024-02-09 13:45:07 +0100

    candidates are sorted now

--- decode.cc
+++ decode.cc
@@ -437,20 +437,15 @@ struct Decoder
 		}
 		*len = data_bits / 8;
 		crc_bits = data_bits + 32;
-		CODE::PolarHelper<mesg_type>::PATH metric[mesg_type::SIZE];
-		polardec(metric, mesg, code, frozen_bits, code_order);
+		polardec(nullptr, mesg, code, frozen_bits, code_order);
 		systematic();
-		int order[mesg_type::SIZE];
-		for (int k = 0; k < mesg_type::SIZE; ++k)
-			order[k] = k;
-		std::sort(order, order+mesg_type::SIZE, [metric](int a, int b){ return metric[a] < metric[b]; });
 		int best = -1;
 		for (int k = 0; k < mesg_type::SIZE; ++k) {
 			crc1.reset();
 			for (int i = 0; i < crc_bits; ++i)
-				crc1(mesg[i].v[order[k]] < 0);
+				crc1(mesg[i].v[k] < 0);
 			if (crc1() == 0) {
-				best = order[k];
+				best = k;
 				break;
 			}
 		}
