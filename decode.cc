d5a0b5c2f20a7579f7f54e395216c8fe8fd1de7a
Ahmet Inan <xdsopl@gmail.com>
2024-03-17 11:55:25 +0100

    moved Schmidl & Cox correlator to own header

--- decode.cc
+++ decode.cc
@@ -9,10 +9,10 @@ Copyright 2021 Ahmet Inan <inan@aicodix.de>
 #include <cstdint>
 #include <cmath>
 namespace DSP { using std::abs; using std::min; using std::cos; using std::sin; }
+#include "schmidl_cox.hh"
 #include "bip_buffer.hh"
 #include "theil_sen.hh"
 #include "xorshift.hh"
-#include "trigger.hh"
 #include "complex.hh"
 #include "permute.hh"
 #include "decibel.hh"
@@ -21,7 +21,6 @@ namespace DSP { using std::abs; using std::min; using std::cos; using std::sin; 
 #include "phasor.hh"
 #include "bitman.hh"
 #include "delay.hh"
-#include "sma.hh"
 #include "wav.hh"
 #include "pcm.hh"
 #include "fft.hh"
@@ -32,128 +31,6 @@ namespace DSP { using std::abs; using std::min; using std::cos; using std::sin; 
 #include "qam.hh"
 #include "polar_tables.hh"
 #include "polar_parity_aided.hh"
-
-template <typename value, typename cmplx, int search_pos, int symbol_len, int guard_len>
-struct SchmidlCox
-{
-	typedef DSP::Const<value> Const;
-	static const int match_len = guard_len | 1;
-	static const int match_del = (match_len - 1) / 2;
-	DSP::FastFourierTransform<symbol_len, cmplx, -1> fwd;
-	DSP::FastFourierTransform<symbol_len, cmplx, 1> bwd;
-	DSP::SMA4<cmplx, value, symbol_len, false> cor;
-	DSP::SMA4<value, value, 2*symbol_len, false> pwr;
-	DSP::SMA4<value, value, match_len, false> match;
-	DSP::Delay<value, match_del> align;
-	DSP::SchmittTrigger<value> threshold;
-	DSP::FallingEdgeTrigger falling;
-	cmplx tmp0[symbol_len], tmp1[symbol_len];
-	cmplx kern[symbol_len];
-	cmplx cmplx_shift = 0;
-	value timing_max = 0;
-	value phase_max = 0;
-	int index_max = 0;
-
-	static int bin(int carrier)
-	{
-		return (carrier + symbol_len) % symbol_len;
-	}
-	static cmplx demod_or_erase(cmplx curr, cmplx prev, value pwr)
-	{
-		if (!(norm(curr) > pwr))
-			return 0;
-		if (!(norm(prev) > pwr))
-			return 0;
-		cmplx cons = curr / prev;
-		if (!(norm(cons) <= 4))
-			return 0;
-		return cons;
-	}
-public:
-	int symbol_pos = 0;
-	value cfo_rad = 0;
-	value frac_cfo = 0;
-
-	SchmidlCox(const cmplx *sequence) : threshold(value(0.17*match_len), value(0.19*match_len))
-	{
-		fwd(kern, sequence);
-		for (int i = 0; i < symbol_len; ++i)
-			kern[i] = conj(kern[i]) / value(symbol_len);
-	}
-	bool operator()(const cmplx *samples)
-	{
-		cmplx P = cor(samples[search_pos+symbol_len] * conj(samples[search_pos+2*symbol_len]));
-		value R = value(0.5) * pwr(norm(samples[search_pos+2*symbol_len]));
-		value min_R = 0.00001 * symbol_len;
-		R = std::max(R, min_R);
-		value timing = match(norm(P) / (R * R));
-		value phase = align(arg(P));
-
-		bool collect = threshold(timing);
-		bool process = falling(collect);
-
-		if (!collect && !process)
-			return false;
-
-		if (timing_max < timing) {
-			timing_max = timing;
-			phase_max = phase;
-			index_max = match_del;
-		} else if (index_max < symbol_len + guard_len + match_del) {
-			++index_max;
-		}
-
-		if (!process)
-			return false;
-
-		frac_cfo = phase_max / value(symbol_len);
-
-		DSP::Phasor<cmplx> osc;
-		osc.omega(frac_cfo);
-		symbol_pos = search_pos - index_max;
-		index_max = 0;
-		timing_max = 0;
-		for (int i = 0; i < symbol_len; ++i)
-			tmp1[i] = samples[i+symbol_pos+symbol_len] * osc();
-		fwd(tmp0, tmp1);
-		value min_pwr = 0;
-		for (int i = 0; i < symbol_len; ++i)
-			min_pwr += norm(tmp0[i]);
-		min_pwr /= symbol_len;
-		for (int i = 0; i < symbol_len; ++i)
-			tmp1[i] = demod_or_erase(tmp0[i], tmp0[bin(i-1)], min_pwr);
-		fwd(tmp0, tmp1);
-		for (int i = 0; i < symbol_len; ++i)
-			tmp0[i] *= kern[i];
-		bwd(tmp1, tmp0);
-
-		int shift = 0;
-		value peak = 0;
-		value next = 0;
-		for (int i = 0; i < symbol_len; ++i) {
-			value power = norm(tmp1[i]);
-			if (power > peak) {
-				next = peak;
-				peak = power;
-				shift = i;
-			} else if (power > next) {
-				next = power;
-			}
-		}
-		if (peak <= next * 4)
-			return false;
-
-		int pos_err = std::nearbyint(arg(tmp1[shift]) * symbol_len / Const::TwoPi());
-		if (abs(pos_err) > guard_len / 2)
-			return false;
-		symbol_pos -= pos_err;
-
-		cfo_rad = shift * (Const::TwoPi() / symbol_len) - frac_cfo;
-		if (cfo_rad >= Const::Pi())
-			cfo_rad -= Const::TwoPi();
-		return true;
-	}
-};
 
 void base37_decoder(char *str, long long int val, int len)
 {
